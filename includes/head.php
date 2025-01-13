c7962ae20de8a5525177b79a6fe46e57f0387d5a
Samuel Osei Adu <asamuel355@yahoo.com>
2025-01-13 10:32:41 +0000

    dream pos first setup with dashboard

---
+++ includes/head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+  <meta name="description" content="Responsive multipurpose POS">
+  <meta name="author" content="Samuel Osei Adu">
+  <title>Dreams Pos</title>
+
+  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+  <link rel="stylesheet" href="assets/css/animate.css">
+
+  <link rel="stylesheet" href="assets/css/dataTables.bootstrap4.min.css">
+
+  <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+  <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
---
+++ includes/sidebar.php
@@ -0,0 +1,55 @@
+<div class="sidebar" id="sidebar">
+  <div class="sidebar-inner slimscroll">
+    <div id="sidebar-menu" class="sidebar-menu">
+      <ul>
+        <li class="submenu-open">
+          <h6 class="submenu-hdr">Main</h6>
+          <ul>
+            <li class="active">
+              <a href="/"><i data-feather="grid"></i><span>Dashboard</span></a>
+            </li>
+            <li class="active">
+            <li><a href="pos"><i data-feather="hard-drive"></i><span>POS</a></li>
+        </li>
+      </ul>
+      </li>
+      <li class="submenu-open">
+        <h6 class="submenu-hdr">Products</h6>
+        <ul>
+          <li><a href=""><i data-feather="box"></i><span>Products</span></a></li>
+          <li><a href=""><i data-feather="plus-square"></i><span>Add Product</span></a></li>
+          <li><a href=""><i data-feather="codepen"></i><span>Category</span></a></li>
+        </ul>
+      </li>
+      <li class="submenu-open">
+        <h6 class="submenu-hdr">Sales</h6>
+        <ul>
+          <li><a href=""><i data-feather="shopping-cart"></i><span>Sales</span></a></li>
+          <li><a href=""><i data-feather="file-text"></i><span>Invoices</span></a></li>
+        </ul>
+      </li>
+      <li class="submenu-open">
+        <h6 class="submenu-hdr">Peoples</h6>
+        <ul>
+          <li><a href=""><i data-feather="user-check"></i><span>Users</span></a></li>
+          <li><a href=""> <i data-feather="user-check"></i> <span>Add User</span> </a></li>
+          <li><a href=""><i data-feather="user"></i><span>Customers</span></a></li>
+        </ul>
+      </li>
+
+
+      <li class="submenu-open">
+        <h6 class="submenu-hdr">Settings</h6>
+        <ul>
+          <li class="submenu">
+            <a href=""><i data-feather="settings"></i><span>Settings</span><span></span></a>
+          </li>
+          <li>
+            <a href=""><i data-feather="log-out"></i><span>Logout</span> </a>
+          </li>
+        </ul>
+      </li>
+      </ul>
+    </div>
+  </div>
+</div>
