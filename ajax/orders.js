fc3bbaecc491896c78d97bd5a62f0c6cf8da08e7
Samuel Osei Adu <asamuel355@yahoo.com>
2025-01-16 10:17:14 +0000

    update on fetching plots every 3 seconds

--- ajax/orders.js
+++ ajax/orders.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Load initial orders
   loadOrders();
+
+  // Set up periodic refresh (every 5 seconds)
+  setInterval(loadOrders, 3000);
 });
 
 //Load Orders
@@ -14,7 +17,7 @@ function loadOrders() {
     })
     .then((data) => {
       if (data.status === "success") {
-        //console.log(data.orders);
+        console.log('fetching...')
         displayOrders(data.orders);
       }
     })
@@ -61,8 +64,14 @@ function displayOrders(orders) {
 
 //Format date
 function formatDate(dateString) {
-  const date = new Date(dateString);
-  return date.toLocaleDateString() + " at " + date.toLocaleTimeString();
+  const options = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  return new Date(dateString).toLocaleDateString("en-US", options);
 }
 
 //Refresh orders when new order is created
@@ -81,6 +90,7 @@ function initializeSSE() {
   };
 }
 
+//Delete order
 function deleteOrder(orderId) {
   if (confirm("Are you sure you want to delete this order?")) {
     fetch("php/delete-order.php", {
@@ -91,15 +101,15 @@ function deleteOrder(orderId) {
       body: JSON.stringify({ order_id: orderId }),
     })
       .then((response) => {
-        console.log(response)
-        return response.json()
+        console.log(response);
+        return response.json();
       })
       .then((data) => {
         if (data.status === "success") {
           loadOrders(); // Reload orders after deletion
           toastr.success("Order deleted successfully");
         } else {
-          console.log(data)
+          console.log(data);
           toastr.error("Error deleting order");
         }
       })
